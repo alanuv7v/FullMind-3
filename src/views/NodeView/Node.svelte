fd6ea2226312918b0e31ee7c67515ed9d16c1d32
alanuv7v <alanuv7v@gmail.com>
2023-09-29 20:10:39 +0900

    good

--- src/views/NodeView/Node.svelte
+++ src/views/NodeView/Node.svelte
@@ -4,6 +4,7 @@
   export let tile
 
   //settings
+    let initGrow = true
     let typewritter = false
 
   let main
@@ -27,11 +28,14 @@
   }
   
   onMount(() => {
-    main.style.width = tile[0] + 'px'
-    main.style.height = tile[1] + 'px'
     if (typewritter) {
       typewrite(main, 'innerText')
     }
+    if (initGrow) {
+      //대충 css 애니메이션. 점에서 가로선이 됬다가 세로로 길어져 열리는.
+    }
+    main.style.minWidth = tile[0] + 'px'
+    main.style.minHeight = tile[1] + 'px'
   })
 
 
@@ -39,12 +43,14 @@
 
 <main bind:this={main} class="border">
   Node
-  <MultilineTextarea placeholder="Heading" color="white"/>
+  <MultilineTextarea placeholder="why am I floating on this earth alone? I always wondered." color="white"/>
 </main>
 
 <style lang="stylus">
   main {
     padding: 10px;
+    overflow: hidden;
+    transition: width 0.5s ease, height 0.5s ease;
   }
   
 </style>
