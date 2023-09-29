09897fe4f19433aab0a21c7d511a9c3c6e08df36
alanuv7v <135824493+alanuv7v@users.noreply.github.com>
2023-09-29 18:36:48 +0900

    1 file modified

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,12 @@
-<!-- UI 라이브러리 참고: https://joyofcode.xyz/svelte-ui-libraries
-특히 요거 맘에듬 https://carbon-components-svelte.onrender.com/components/Tooltip
-그 외
-https://illright.github.io/attractions//docs/components/text-field
-https://daisyui.com/components/
-
-라이브러리의 장점은 잘 알겠다. 그러나 단점이 있다면?:
-https://armadillo-dev.github.io/ui/ux/benefit-and-cost-of-ui-component-framework/
-
-...결국 CSS가 중요한 상황이 오기 전까진 일단 쓰지 말자.
-로직부터 짜고, 나중에 생각하자. UI 라이브러리는 미관을 고려할 때 사용하자. 지금은 미관은 제껴두자.
-
--->
 <script>
-import Fontfetcher from "./Fontfetcher.svelte"
-import Header from "./Header.svelte"
-import NodeView from "./views/NodeView/NodeView.svelte"
-import MultilineTextarea from './lib/MultilineTextarea.svelte'
-import {getContext, onMount} from 'svelte'
+//import
+  import Fontfetcher from "./Fontfetcher.svelte"
+  import Header from "./Header.svelte"
+  import NodeView from "./views/NodeView/NodeView.svelte"
+  import MultilineTextarea from './lib/MultilineTextarea.svelte'
+  import {getContext, onMount} from 'svelte'
 
-import * as glob from './themes/green_cozy/global_variables.json';
+  import * as glob from './themes/green_cozy/global_variables.json';
 
 
 let container = {
@@ -26,39 +14,45 @@ let container = {
   thot: {id: 5, heading: '3th', content: 'thot 1.1.1', children: []}
 }
 
-let i = 1
-let hi = window.innerHeight
-let hi2 = window.outerHeight
-let hi3 = document.documentElement.clientHeight
-let hi4 = window.visualViewport.height
+//keyboard_toolbar
+  let keyboard_toolbar;
+  function locate_keyboard_toolbar() {
+    keyboard_toolbar.style.top = (window.visualViewport.height - Number(getComputedStyle(keyboard_toolbar).height.match(/(\d+)/)[0])) +"px"
+  }
+  visualViewport.onresize = () => {
+    locate_keyboard_toolbar()
+  };
 
-let keyboard_toolbar;
-function locate_keyboard_toolbar() {
-  keyboard_toolbar.style.top = (window.visualViewport.height - Number(getComputedStyle(keyboard_toolbar).height.match(/(\d+)/)[0])) +"px"
-}
-visualViewport.onresize = () => {
-  locate_keyboard_toolbar()
-};
+  onMount(() => {
+    locate_keyboard_toolbar()
+  })
 
-onMount(() => {
-  locate_keyboard_toolbar()
-})
 
-//setInterval(() => alert(hi + "/" + hi2 + "/" + hi4), 10000);
 
-let IndentedThotsView_elem;
-let command_palette;
+//command_pallette
+  let command_palette;
 
-window.onkeydown = function(e) {
-  if ((e.key === "p")&&(e.altKey)) {
-    if (command_palette.style.display === "none") {
-      command_palette.style.display = "inline-block"
-    }
-    else {
-      command_palette.style.display = "none"
+  window.onkeydown = function(e) {
+    if ((e.key === "p")&&(e.altKey)) {
+      if (command_palette.style.display === "none") {
+        command_palette.style.display = "inline-block"
+      }
+      else {
+        command_palette.style.display = "none"
+      }
     }
   }
-}
+
+
+//debug
+  
+  let i = 1
+  let hi = window.innerHeight
+  let hi2 = window.outerHeight
+  let hi3 = document.documentElement.clientHeight
+  let hi4 = window.visualViewport.height
+
+
 
 </script>
 
@@ -77,7 +71,7 @@ window.onkeydown = function(e) {
     `command pallete...
 (search a thot by typing its heading / specific prop value / directory, modify settings, ect.)`
     }
-    color={glob.light_green}
+    color={glob.light}
     />
   </div>
   <Header />
@@ -91,7 +85,6 @@ window.onkeydown = function(e) {
     The quick brown fox jumps over the lazy dog
     다람쥐 헌 쳇바퀴에 타고파
     !@#$%^&*
-    asdfsaf
     <div>{hi} / {hi2} / {hi3} / {hi4}</div>
   </div>
   
