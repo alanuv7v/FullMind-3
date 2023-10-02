ce265f7de492352e0b857ad3151d3dfcb5a23607
alanuv7v <alanuv7v@gmail.com>
2023-10-02 16:15:21 +0900

    eeentry

--- src/views/NodeView/Entry.svelte
+++ src/views/NodeView/Entry.svelte
@@ -1,9 +1,16 @@
 <script>
+  import MultilineTextarea from "../../lib/MultilineTextarea.svelte";
+  import {createEventDispatcher} from 'svelte'
   export let data
+
+  const dispatch = createEventDispatcher()
 
 </script>
 <div class="entry">
-  {input}
+  <MultilineTextarea key={data.key} placeholder={data.key} value={data.value} 
+  color="white" textAlign={data.style.textAlign} 
+  on:focus={() => dispatch('onTextareaFocusdispatch')} 
+  on:input={() => dispatch('onTextareaInput')}/>
   <div id="util">
     <button id="addProp">+</button>
     <button id="delProp">-</button>
