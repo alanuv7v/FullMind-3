346da2ff5159832c9a30351089ff2eccee2678d0
alanuv7v <alanuv7v@gmail.com>
2023-10-02 16:01:20 +0900

    ENTRY

---
+++ src/views/NodeView/Entry.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let data
+
+</script>
+<div class="entry">
+  {input}
+  <div id="util">
+    <button id="addProp">+</button>
+    <button id="delProp">-</button>
+  </div>
+</div>
+
+<style>
+  .entry {
+    display: flex;
+    flex-direction: row;
+  }
+  #util {
+    display: flex;
+    flex-direction: column;
+  } 
+</style>
