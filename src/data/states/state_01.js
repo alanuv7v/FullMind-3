1fd532f0f5fb0041bb1b06e986f091ee32afcda9
alanuv7v <alanuv7v@gmail.com>
2023-10-15 17:58:02 +0900

    asdf

--- src/data/states/state_01.js
+++ src/data/states/state_01.js
@@ -1,4 +1,4 @@
 export let state = {
-  loadedHeadPath: "/home/alanuv7v/FullMind-3/src/data/heads/head_1.json", //절대경로로 바꿔야 할듯?
+  loadedHeadPath: "../heads/head_1.json", //절대경로로 바꿔야 할듯?
   loadedHead: null
 }
