bae2e5bb58717183367d81a169a19814a3631c7e
alanuv7v <alanuv7v@gmail.com>
2023-10-15 18:01:09 +0900

    sfda

--- src/lib/importModule.js
+++ src/lib/importModule.js
@@ -1,8 +1,9 @@
 export default async function importModule(path) {
-  try {
+  return await import(path)
+  /* try {
     const module = await import(path)
     return module
   } catch (error) {
       console.error('import failed');
-  }
+  } */
 }
